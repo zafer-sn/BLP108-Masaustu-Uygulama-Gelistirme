6cd67bb2cd2aaec0564e57d65af524c96043a718
Zafer <162111813+zafer-sn@users.noreply.github.com>
2026-03-05 17:16:17 +0300

    h1-2

---
+++ Hafta2/Kodlar/Program.cs
@@ -0,0 +1,42 @@
+﻿// Bu Program classidir.
+internal class Program
+{
+    // Burasi Main fonksiyonudur.
+    private static void Main(string[] args)
+    {
+        /*
+         Bu kod
+        ekrana
+        Test yazar         
+         */
+        Console.WriteLine("Test"); // Deneme
+        /* Console.WriteLine(args[0]);
+        Console.WriteLine(args[1]);
+        Console.WriteLine(args[2]); */
+        #region Degiskenler
+        Console.WriteLine("Degiskenler");
+        #endregion
+        #region Donguler
+
+        #endregion
+        #region Diziler
+
+        #endregion
+        //todo Buradan sonrasini Suheyla yazacak
+
+
+
+
+
+        //TODO Burasini Acelya yazacak
+        // Deneme test
+        for (int i = 1; i < 1000; i++)
+        {
+            if(i % 2 == 0)
+            {
+                Console.WriteLine(i * i);
+            }
+
+        }
+    }
+}
